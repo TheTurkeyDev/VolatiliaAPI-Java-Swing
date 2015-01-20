aa59bf1532f61bf7db737463c5521bce31b1a9b0
Turkey2349 <turkey2349@gmail.com>
2015-01-20 10:52:33 -0500

    Added size scaling to fonts

--- src/VolatiliaAPI/graphics/Font.java
+++ src/VolatiliaAPI/graphics/Font.java
@@ -5,22 +5,28 @@ public class Font
 
 	private ImageSheet fontSheet;
 	private int size;
+	private int incSize;
 	private Image[] letters = new Image[96];
 	
 	public Font(ImageSheet fs, int s)
 	{
 		fontSheet = fs;
 		size = s;
+		incSize = size;
 		loadCharecters();
 	}
 	
 	private void loadCharecters()
 	{
-		for(int y = 0; y < (fontSheet.getHeight() / size) ; y++)
+		for(int y = 0; y < (fontSheet.getHeight()/size); y++)
 		{
-			for(int x = 0; x < (fontSheet.getWidth() / size); x++)
+			for(int x = 0; x < (fontSheet.getWidth()/size); x++)
 			{
 				Image image = new Image(fontSheet, x * size, y * size, size, size);
+				if(incSize != size)
+				{
+					image = image.scale(incSize);
+				}
 				letters[(fontSheet.getWidth() / size) * y + x] = image;
 			}
 		}
@@ -34,6 +40,13 @@ public class Font
 			stringImages[i] = letters[s.charAt(i) - 32];
 		}
 		return stringImages;
+	}
+	
+	public Font setMultiplySize(int s)
+	{
+		incSize = s;
+		loadCharecters();
+		return this;
 	}
 	
 }
